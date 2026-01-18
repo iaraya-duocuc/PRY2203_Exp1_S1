d163511c1a82dafc2fedf3ba8c7e8cb6f3543ae5
Ignacio Araya <ign.arayaa@duocuc.cl>
2026-01-18 15:06:34 -0300

    Update Pedido.java
    Clase ahora es abstracta, se agregan dos métodos nuevos, más el atributo de distancia. Se reemplazaron modificadores de acceso a private, agregando getters para mayor claridad.

--- src/main/java/cl/speedfast/model/Pedido.java
+++ src/main/java/cl/speedfast/model/Pedido.java
@@ -2,16 +2,24 @@ package cl.speedfast.model;
 
 import cl.speedfast.config.AppConfig;
 
-public class Pedido {
-    protected int idPedido;
-    protected String direccion;
-    protected AppConfig.TipoPedido tipoPedido;
+public abstract class Pedido {
+    private int idPedido;
+    private String direccion;
+    private AppConfig.TipoPedido tipoPedido;
+    private double distanciaKm;
 
-    public Pedido(int idPedido, String direccion, AppConfig.TipoPedido tipoPedido) {
+    public Pedido(int idPedido, String direccion, AppConfig.TipoPedido tipoPedido, double distanciaKm) {
         this.idPedido = idPedido;
         this.direccion = direccion;
         this.tipoPedido = tipoPedido;
+        this.distanciaKm = distanciaKm;
     }
+
+    // Getters
+    public int getIdPedido() { return idPedido; }
+    public String getDireccion() { return direccion; }
+    public AppConfig.TipoPedido getTipoPedido() { return tipoPedido; }
+    public double getDistanciaKm() { return distanciaKm; }
 
     // Para sobreescritura
     public void asignarRepartidor() {
@@ -21,5 +29,14 @@ public class Pedido {
     // Sobrecarga
     public void asignarRepartidor(String nombreRepartidor) {
         System.out.println("Repartidor " + nombreRepartidor + " asignado al pedido " + idPedido);
+    }
+
+    // Abstracta ya que no hay fórmula genérica aplicable a todas las clases que heredan de Pedido
+    public abstract int calcularTiempoEntrega();
+
+    public void mostrarResumen() {
+        System.out.println("Pedido " + tipoPedido.obtenerNombre() + " #" + idPedido);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Distancia: " + distanciaKm + " km");
     }
 }
