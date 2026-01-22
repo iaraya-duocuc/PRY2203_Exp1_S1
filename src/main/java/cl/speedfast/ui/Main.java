9009b5e8655cce8d2e67599a6abc645395089892
Ignacio Araya <ign.arayaa@duocuc.cl>
2026-01-22 14:08:31 -0300

    Update Main.java
    Actualizaciones para reflejar nuevas funcionalidades

--- src/main/java/cl/speedfast/ui/Main.java
+++ src/main/java/cl/speedfast/ui/Main.java
@@ -15,27 +15,45 @@ public class Main {
         Pedido encomienda = new PedidoEncomienda(2, "Calle Sur 456", 10, true, 6.0);
         Pedido express = new PedidoExpress(3, "Los Pinos 789", false, 1.1);
 
-        // Override & overload
-        /*comida.asignarRepartidor();
-        comida.asignarRepartidor("Carlos");
-        System.out.println();
-        encomienda.asignarRepartidor();
-        encomienda.asignarRepartidor("María");
-        System.out.println();
-        express.asignarRepartidor();
-        express.asignarRepartidor("Pedro");*/
-
         // Se agregan los pedidos a una colección para mayor flexibilidad
         List<Pedido> pedidos = new ArrayList<>();
         pedidos.add(comida);
         pedidos.add(encomienda);
         pedidos.add(express);
 
-        // Se recorre colección para demostrar polimorfismo
+        // Pedido Comida
+        comida.mostrarResumen();
+        comida.asignarRepartidor("Luis Perales");
+        System.out.println("Tiempo estimado: " +
+                comida.calcularTiempoEntrega() + " minutos");
+        comida.despachar();
+
+        System.out.println();
+
+        // Pedido Encomienda
+        encomienda.mostrarResumen();
+        encomienda.asignarRepartidor("Daniela Tapia");
+        System.out.println("Tiempo estimado: " +
+                encomienda.calcularTiempoEntrega() + " minutos");
+        encomienda.despachar();
+
+        System.out.println();
+
+        // Pedido Express (ej. cancelado)
+        express.mostrarResumen();
+        express.asignarRepartidor("Pedro Soto");
+        System.out.println("Tiempo estimado: " +
+                express.calcularTiempoEntrega() + " minutos");
+        express.cancelar();
+
+        // Historial de despachos
+        System.out.println();
+        System.out.println("Historial:");
+
         for (Pedido p : pedidos) {
-            p.mostrarResumen();
-            System.out.println("Tiempo estimado: " + p.calcularTiempoEntrega() + " minutos");
-            System.out.println("-----------------------------");
+            for (String evento : p.verHistorial()) {
+                System.out.println("- " + evento);
+            }
         }
 
     }
